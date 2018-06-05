b46394034182455ba2bba385df85e2de644e72f8
Tommaso Ricci <t.ricci@molecle.com>
2018-06-05 18:51:25 +0200

    #WIP map + fixes

--- src/enumValues.ts
+++ src/enumValues.ts
@@ -1,5 +1,8 @@
+import 'core-js/es6/map';
+
 export type EnumValueType = string | number;
 
+// @TODO: fix names from values should be an array instead of a single element
 export class EnumValues {
   static getNamesAndValues<T extends EnumValueType>(e: any): { name: string, value: T }[] {
     return this.getNames(e).map(_name => { return { name: _name, value: e[_name] as T }; });
@@ -17,4 +20,14 @@ export class EnumValues {
   static getValues<T extends EnumValueType>(e: any): T[] {
     return this.getNames(e).map(name => e[name]) as T[];
   }
+
+  // @TODO: fix ES6 Map missing typing
+  // static getNameMap<T extends EnumValueType>(e: any): Map<string, T> {
+  //   return new Map(this.getNamesAndValues(e).map(pair => [pair.name, pair.value]));
+  // }
+
+  // @TODO: add getValueMap
+  // static getValueMap<T extends EnumValueType>(e: any): Map<T, string[]> {
+  //   // return new Map(this.getNamesAndValues(e).map(pair => [pair.name, pair.value]));
+  // }
 }
